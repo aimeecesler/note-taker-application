0ff7ebb62876df364089eb7aa690d0f4a3ea2f6e
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-09-30 17:59:09 -0400

    fixed pathways so js and css files work correctly

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // ARRAY
 const noteData = [];
@@ -14,22 +14,27 @@ const noteData = [];
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./db/db.json"));
-// return res.json(noteData);
+  return res.sendFile(path.join(__dirname, "db/db.json"));
+  // return res.json(noteData);
 });
+
+// app.post("/api/notes", function (req, res) {
+
+// });
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function () {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on http://localhost:" + PORT);
 });
